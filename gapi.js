3068e6af3b8007e9f0930504fd6a3cb7d3292eb5
radishengine <radishengine@gmail.com>
2017-08-06 19:27:47 +0100

    Update gapi.js

--- gapi.js
+++ gapi.js
@@ -1,11 +1,12 @@
 
-define([], function() {
+define(['//apis.google.com/js/api.js'], function() {
   
   'use strict';
   
   var gapi = window.gapi;
   
   return {
+    module: gapi,
     load: function(name, parentRequire, onload, config) {
       gapi.load(name, {
         callback: onload,
