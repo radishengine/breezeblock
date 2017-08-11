721df3cbe5f2e06dde1954f29e4e4ebd7fe0e69e
radishengine <radishengine@gmail.com>
2017-08-11 18:11:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ requirejs(['domReady!', 'gapi!auth2:client,drive-realtime'], function() {
   'use strict';
   
   var gapi = window.gapi;
-  
+
+  var DEV_KEY = 'AIzaSyD__sT53UZduSvg5E2rH6L8x-dPzimBV7k';
+  var APP_ID = "908558406138";
   var CLIENT_ID = "908558406138-rmva8p8ubsjppi6fr7l9cks4mlpi4hs1.apps.googleusercontent.com";
   var DISCOVERY_DOCS = [
     "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
@@ -28,13 +30,25 @@ requirejs(['domReady!', 'gapi!auth2:client,drive-realtime'], function() {
   function updateSigninStatus(isSignedIn) {
     if (isSignedIn) {
       document.body.classList.add('logged-in');
+      redrawPage();
     }
     else {
       document.body.classList.remove('logged-in');
     }
   }
   
+  function redrawPage() {
+    var existing = location.hash.match(/^#\/?([^\/]+)(?:\/([^\/]+))?\/?$/);
+    if (!existing) {
+      listZips();
+    }
+    else {
+      
+    }
+  }
+  
   function updateUser(user) {
+    /*
     console.log(user.getId());
     var basicProfile = user.getBasicProfile();
     console.log(basicProfile.getId());
@@ -42,6 +56,7 @@ requirejs(['domReady!', 'gapi!auth2:client,drive-realtime'], function() {
     console.log(basicProfile.getGivenName());
     console.log(basicProfile.getFamilyName());
     console.log(basicProfile.getEmail());
+    */
   }
   
   function getFolderTree() {
@@ -134,86 +149,15 @@ requirejs(['domReady!', 'gapi!auth2:client,drive-realtime'], function() {
     clientId: CLIENT_ID,
     scope: SCOPES,
   }).then(function() {
-    document.body.classList.remove('loading');
     // Listen for sign-in state changes.
     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
     gapi.auth2.getAuthInstance().currentUser.listen(updateUser);
+    
     // Handle the initial sign-in state.
     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
     updateUser(gapi.auth2.getAuthInstance().currentUser.get());
-    listZips();
-  });
-  
-  /*
-  var developerKey = 'AIzaSyD__sT53UZduSvg5E2rH6L8x-dPzimBV7k';
-  var clientId = "908558406138-rmva8p8ubsjppi6fr7l9cks4mlpi4hs1.apps.googleusercontent.com";
-  var appId = "908558406138";
-  var scope = ['https://www.googleapis.com/auth/drive'];
-  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
-
-  var authorizeButton = document.getElementById('authorize-button');
-  var signoutButton = document.getElementById('signout-button');
-
-  gapi.client.init({
-    discoveryDocs: DISCOVERY_DOCS,
-    clientId: clientId,
-    scope: scope,
   }).then(function() {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-  },
-  function(reason) {
-    console.error(reason);
+    document.body.classList.remove('loading');
   });
-
-  function updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-      authorizeButton.style.display = 'none';
-      signoutButton.style.display = 'block';
-      listFiles();
-    } else {
-      authorizeButton.style.display = 'block';
-      signoutButton.style.display = 'none';
-    }
-  }
-
-  function handleAuthClick(event) {
-    gapi.auth2.getAuthInstance().signIn();
-  }
-
-  function handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
-  }
-
-  function appendPre(message) {
-    var pre = document.getElementById('content');
-    var textContent = document.createTextNode(message + '\n');
-    pre.appendChild(textContent);
-  }
-
-  function listFiles() {
-    gapi.client.drive.files.list({
-      'pageSize': 10,
-      'fields': "nextPageToken, files(id, name)"
-    }).then(function(response) {
-      appendPre('Files:');
-      var files = response.result.files;
-      if (files && files.length > 0) {
-        for (var i = 0; i < files.length; i++) {
-          var file = files[i];
-          appendPre(file.name + ' (' + file.id + ')');
-        }
-      } else {
-        appendPre('No files found.');
-      }
-    });
-  }
-  
-  */
   
 });
